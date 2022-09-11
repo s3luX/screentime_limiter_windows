71a4454c13ce8ad723141192ac33b87716605b86
Andrej <s3luX@users.noreply.github.com>
2022-09-11 23:54:18 +0200

    Add files via upload

---
+++ pass_change.ps1
@@ -0,0 +1,10 @@
+﻿$password = Read-Host -AsSecureString "Enter new password"
+$Bytes = [System.Text.Encoding]::Unicode.GetBytes($password)
+$EncodedText =[Convert]::ToBase64String($Bytes)
+cls
+Write-Host "New encrypted password is"
+$EncodedText
+Write-Host
+Write-Host "Opening notepad and file, please change the entry in the file on line 14"
+Start-Process notepad "$env:APPDATA/stopper.ps1"
+Read-Host
